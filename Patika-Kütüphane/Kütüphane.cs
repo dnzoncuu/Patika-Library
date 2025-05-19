34d52b9eeffb75ce903878bb7f894beeb704f99a
dnzoncuu <berkoncu17@gmail.com>
2025-05-19 14:45:37 +0300

    Add project files.

---
+++ Patika-Kütüphane/Kütüphane.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patika_Kütüphane
+{
+    public class Kütüphane//Kütüphane adında sınıfımızı oluşturduk.
+    {
+        //Fields(Alanlar)
+        private string _kitapAd;
+        private string _yazarAd;
+        private string _yazarSoyAd;
+        private int _sayfaSayisi;
+        private string _yayinEvi;
+        private DateTime _kayitTarihi;
+
+        //Properties
+        public string KitapAd { get { return _kitapAd; } set { _kitapAd = value; } }
+        public string YazarAd { get { return _yazarAd; } set { _yazarAd = value; } }
+        public string YazarSoyAd { get { return _yazarSoyAd; } set { _yazarSoyAd = value; } }
+        public int SayfaSayisi { get { return _sayfaSayisi; } set { _sayfaSayisi = value; } }
+        public string YayinEvi { get { return _yayinEvi; } set { _yayinEvi = value; } }
+        public DateTime KayitTarihi { get { return _kayitTarihi; } set { _kayitTarihi = value; } }
+
+        // Null constructor
+        public Kütüphane()
+        {
+            _kayitTarihi = DateTime.Now;
+        }
+        //constructor
+        public Kütüphane(string kitapAd,string yazarAd,string yazarSoyAd,int sayfaSayisi,string yayinEvi)
+        {
+            _kitapAd=kitapAd;
+            _yazarAd = yazarAd;
+            _yazarSoyAd=yazarSoyAd;
+            _sayfaSayisi=sayfaSayisi;
+            _yayinEvi=yayinEvi;
+            _kayitTarihi=DateTime.Now;
+        }
+
+        //Method
+        public void BilgileriYazdir()
+        {
+            Console.WriteLine($" Kitabın Adı :{_kitapAd} , Yazarın Adı : {_yazarAd} , Yazarın Soyadı : {_yazarSoyAd} , Sayfa sayısı : {_sayfaSayisi} , Yayın evi : {_yayinEvi} , Kayıt Tarihi : {_kayitTarihi.ToShortDateString()}");
+        }
+    }
+}
---
+++ Patika-Kütüphane/Program.cs
@@ -0,0 +1,18 @@
+﻿using Patika_Kütüphane;
+
+
+
+Kütüphane kitap1 = new Kütüphane("Korku","osho","-",234,"Öncü yayınevi");
+kitap1.BilgileriYazdir();
+
+Kütüphane kitap2 = new Kütüphane("Kördüğüm","Ayşe","Kulin",352,"Öncü yayınevi");
+kitap2.BilgileriYazdir();
+
+Kütüphane kitap3 = new Kütüphane();
+
+kitap3.KitapAd = "The Witcher";
+kitap3.YazarAd = "Andrzej";
+kitap3.YazarSoyAd = "Sapkowski";
+kitap3.SayfaSayisi = 440;
+kitap3.BilgileriYazdir();
+
